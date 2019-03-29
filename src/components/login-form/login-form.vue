b859f2e27811bc7eda70814b6f6fc0d6f9462272
renhy <renhy@ziroom.com>
2019-03-29 17:45:08 +0800

    更新样式

--- src/components/login-form/login-form.vue
+++ src/components/login-form/login-form.vue
@@ -1,21 +1,21 @@
 <template>
   <Form ref="loginForm" :model="form" :rules="rules" @keydown.enter.native="handleSubmit">
     <FormItem prop="userName">
-      <Input v-model="form.userName" placeholder="请输入用户名">
+      <Input v-model="form.userName" size="large" placeholder="请输入用户名">
         <span slot="prepend">
           <Icon :size="16" type="ios-person"></Icon>
         </span>
       </Input>
     </FormItem>
     <FormItem prop="password">
-      <Input type="password" v-model="form.password" placeholder="请输入密码">
+      <Input type="password" v-model="form.password" size="large" placeholder="请输入密码">
         <span slot="prepend">
           <Icon :size="14" type="md-lock"></Icon>
         </span>
       </Input>
     </FormItem>
     <FormItem>
-      <Button @click="handleSubmit" type="primary" long>登录</Button>
+      <Button @click="handleSubmit" type="primary" size="large" long>登录</Button>
     </FormItem>
   </Form>
 </template>
