ff727192b08379d3e1085df473746bda1f6599ae
layme <1149319668@qq.com>
2019-06-01 01:58:45 +0800

    模拟登录

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -1,5 +1,5 @@
 import {
-  login,
+  // login,
   logout,
   getUserInfo,
   getMessage,
@@ -77,17 +77,20 @@ export default {
     // 登录
     handleLogin ({ commit }, { userName, password }) {
       userName = userName.trim()
-      return new Promise((resolve, reject) => {
-        login({
-          userName,
-          password
-        }).then(res => {
-          const data = res.data
-          commit('setToken', data.token)
-          resolve()
-        }).catch(err => {
-          reject(err)
-        })
+      // return new Promise((resolve, reject) => {
+      //   login({
+      //     userName,
+      //     password
+      //   }).then(res => {
+      //     const data = res.data
+      //     commit('setToken', data.token)
+      //     resolve()
+      //   }).catch(err => {
+      //     reject(err)
+      //   })
+      // })
+      return new Promise(() => {
+        commit('setToken', '345asd7sf89a')
       })
     },
     // 退出登录
